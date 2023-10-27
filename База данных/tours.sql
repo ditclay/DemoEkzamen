b2273dc981a71950476b2cb339da5224e3acc015
ditclay <59621706+ditclay@users.noreply.github.com>
2023-10-27 15:53:38 +0300

    Create tours.sql

---
+++ База данных/tours.sql
@@ -0,0 +1,153 @@
+﻿USE [tour_agenstvo]
+GO
+/****** Object:  Table [dbo].[Country]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Country](
+	[id] [int] NOT NULL,
+	[name] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Country] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Hotel]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Hotel](
+	[id] [int] NOT NULL,
+	[name] [nvarchar](100) NULL,
+	[count_of_stars] [int] NULL,
+	[id_country] [int] NULL,
+ CONSTRAINT [PK_Hotel] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[HotelComment]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[HotelComment](
+	[id] [int] NOT NULL,
+	[id_hotel] [int] NULL,
+	[text] [nvarchar](100) NULL,
+	[author] [nvarchar](100) NULL,
+	[creation_date] [datetime] NULL,
+ CONSTRAINT [PK_HotelComment] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[HotelImage]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[HotelImage](
+	[id] [int] NOT NULL,
+	[id_hotel] [int] NULL,
+	[imagesource] [varbinary](max) NULL,
+ CONSTRAINT [PK_HotelImage] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[HotelOfTour]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[HotelOfTour](
+	[id_hotel] [int] NULL,
+	[id_tour] [int] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Tour]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Tour](
+	[id] [int] NOT NULL,
+	[ticketcount] [int] NULL,
+	[name] [nvarchar](100) NULL,
+	[description] [nvarchar](max) NULL,
+	[imagepreview] [varbinary](max) NULL,
+	[price] [money] NULL,
+	[isactual] [bit] NULL,
+ CONSTRAINT [PK_Tour] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Type]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Type](
+	[id] [int] NOT NULL,
+	[name] [nvarchar](100) NULL,
+	[description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Type] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[TypeOfTour]    Script Date: 20.10.2023 9:59:25 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TypeOfTour](
+	[id_tour] [int] NULL,
+	[id_type] [int] NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Hotel]  WITH CHECK ADD  CONSTRAINT [FK_Hotel_Country] FOREIGN KEY([id_country])
+REFERENCES [dbo].[Country] ([id])
+GO
+ALTER TABLE [dbo].[Hotel] CHECK CONSTRAINT [FK_Hotel_Country]
+GO
+ALTER TABLE [dbo].[HotelComment]  WITH CHECK ADD  CONSTRAINT [FK_HotelComment_Hotel] FOREIGN KEY([id_hotel])
+REFERENCES [dbo].[Hotel] ([id])
+GO
+ALTER TABLE [dbo].[HotelComment] CHECK CONSTRAINT [FK_HotelComment_Hotel]
+GO
+ALTER TABLE [dbo].[HotelImage]  WITH CHECK ADD  CONSTRAINT [FK_HotelImage_Hotel] FOREIGN KEY([id_hotel])
+REFERENCES [dbo].[Hotel] ([id])
+GO
+ALTER TABLE [dbo].[HotelImage] CHECK CONSTRAINT [FK_HotelImage_Hotel]
+GO
+ALTER TABLE [dbo].[HotelOfTour]  WITH CHECK ADD  CONSTRAINT [FK_HotelOfTour_Hotel] FOREIGN KEY([id_hotel])
+REFERENCES [dbo].[Hotel] ([id])
+GO
+ALTER TABLE [dbo].[HotelOfTour] CHECK CONSTRAINT [FK_HotelOfTour_Hotel]
+GO
+ALTER TABLE [dbo].[HotelOfTour]  WITH CHECK ADD  CONSTRAINT [FK_HotelOfTour_Tour] FOREIGN KEY([id_tour])
+REFERENCES [dbo].[Tour] ([id])
+GO
+ALTER TABLE [dbo].[HotelOfTour] CHECK CONSTRAINT [FK_HotelOfTour_Tour]
+GO
+ALTER TABLE [dbo].[TypeOfTour]  WITH CHECK ADD  CONSTRAINT [FK_TypeOfTour_Tour] FOREIGN KEY([id_tour])
+REFERENCES [dbo].[Tour] ([id])
+GO
+ALTER TABLE [dbo].[TypeOfTour] CHECK CONSTRAINT [FK_TypeOfTour_Tour]
+GO
+ALTER TABLE [dbo].[TypeOfTour]  WITH CHECK ADD  CONSTRAINT [FK_TypeOfTour_Type] FOREIGN KEY([id_type])
+REFERENCES [dbo].[Type] ([id])
+GO
+ALTER TABLE [dbo].[TypeOfTour] CHECK CONSTRAINT [FK_TypeOfTour_Type]
+GO
